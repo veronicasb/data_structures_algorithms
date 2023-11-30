162e0da6814d26a7660a56d94eebdc9e1907ed48
Veronica Burgess <burgess.veronicas@gmail.com>
2023-11-29 22:03:53 -0700

    Continue "Binary Search, LInked Lists, Complexity"
    Of "Data Structures and Algorithms in Python" (freeCodeCamp.org)

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,33 @@
 # Binary Search, Linked Lists, Complexity Analysis
+
+'''
+
+STRATEGY
+
+1. State problem, identify input and output formats
+2. Brainstorm example inputs and outputs that cover all edge cases
+3. Brainstorm a solution for the problem in plain english
+4. Implement solution and text example inputs. Fix bugs
+5. Analyze complexity, identify inefficiencies. 
+6. Apply technique to overcome inefficiencies - this is where DSA becomes significant. Repeat 3-6
+
+'''
+
+'''
+PROBLEM
+
+Lucy has cards with numbers on them - they are arranged in decreasing order and turned around 
+(we don't know the specific numbers). She challenges Jose to pick a given number from the cards in as
+little moves as possible. Write a function to help Jose.
+
+'''
+
+'''
+
+DSA is crucial because it helps you:
+1. To think about problems systematically
+2. To envision different inputs, outputs , and edge cases
+3. Communicate ideas clearly to colleagues and collaborate
+4. Convert thoughts and ideas into code
+
+'''
