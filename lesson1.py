bd6debe24acbf7b4d4512eeb386d4809594a8e8a
Veronica Burgess <burgess.veronicas@gmail.com>
2023-12-01 23:50:00 -0700

    Finished strategy step 1 for first example problem
    Of "Data Structures and Algorithms in Python" from freeCodeCamp.org

--- lesson1.py
+++ lesson1.py
@@ -2,6 +2,16 @@
 
 '''
 
+DSA is crucial because it helps you:
+1. To think about problems systematically
+2. To envision different inputs, outputs , and edge cases
+3. Communicate ideas clearly to colleagues and collaborate
+4. Convert thoughts and ideas into code
+
+'''
+
+'''
+
 STRATEGY
 
 1. State problem, identify input and output formats
@@ -20,18 +30,35 @@ Lucy has cards with numbers on them - they are arranged in decreasing order and 
 (we don't know the specific numbers). She challenges Jose to pick a given number from the cards in as
 little moves as possible. Write a function to help Jose.
 
+1. We can represent the sequence of cards as a sorted list. So, in other words, write a function to find the
+position of a given number in a sorted list of numbers. We need to minimize the number of times we access the list.
+Our inputs include a sequence of cards (a list), a card (some number to be determined), and our output is a position (
+the index of the card to be determined).
+
+Input and output allows us to create the signature of our function:
+
+2. Example inputs and outputs = test cases. 
 '''
 
-'''
+# Step 1:
+def locate_cards(cards, num):
+    pass
 
-DSA is crucial because it helps you:
-1. To think about problems systematically
-2. To envision different inputs, outputs , and edge cases
-3. Communicate ideas clearly to colleagues and collaborate
-4. Convert thoughts and ideas into code
+# Step 2:
+cards = [13, 11, 10, 7, 4, 3, 1, 0]
+num = 7
+output = 3
 
-'''
+result = locate_cards(cards, num)
+if result == output:
+    print(result)
 
-'''
+# alternatively
 
-'''
+test_cases = {
+    'input': {
+        'cards': [13, 11, 10, 7, 4, 3, 1, 0],
+        'num': 7
+    },
+    'output': 3
+}
