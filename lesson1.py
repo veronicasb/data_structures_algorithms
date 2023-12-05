11adbfa35a260eb1c3352ad11304a8b1a29461f1
Veronica Burgess <burgess.veronicas@gmail.com>
2023-12-04 19:59:38 -0700

    Finished strategy step 2 for first example problem
    Of "Data Structures and Algorithms in Python" from freeCodeCamp.org

--- lesson1.py
+++ lesson1.py
@@ -15,7 +15,7 @@ DSA is crucial because it helps you:
 STRATEGY
 
 1. State problem, identify input and output formats
-2. Brainstorm example inputs and outputs that cover all edge cases
+2. Brainstorm example inputs and outputs that cover all edge cases (extreme/rare examples)
 3. Brainstorm a solution for the problem in plain english
 4. Implement solution and text example inputs. Fix bugs
 5. Analyze complexity, identify inefficiencies. 
@@ -37,14 +37,21 @@ the index of the card to be determined).
 
 Input and output allows us to create the signature of our function:
 
-2. Example inputs and outputs = test cases. 
-'''
+2. Example inputs and outputs => test cases. Possible cases to consider when testing: num occurs somewhere in the middle
+of cards, num is the first element in cards, num is the last element in cards, cards contains only num as an element, cards does 
+not contain num, cards is an empty list, cards contains repeating numbers, or num occurs more than once in cards.
 
+
+'''
+###
 # Step 1:
+###
 def locate_cards(cards, num):
     pass
 
+###
 # Step 2:
+###
 cards = [13, 11, 10, 7, 4, 3, 1, 0]
 num = 7
 output = 3
@@ -53,12 +60,95 @@ result = locate_cards(cards, num)
 if result == output:
     print(result)
 
-# alternatively
-
-test_cases = {
+# alternatively - using dictionaries makes repetitive testing easier
+# input key will contain 1 key for each argument to the function
+test_case = {
     'input': {
         'cards': [13, 11, 10, 7, 4, 3, 1, 0],
         'num': 7
     },
     'output': 3
 }
+
+# to test
+# remember that ** unpacks keyword arguments (kwargs)
+locate_cards(**test_case['input']) == test_case['output']
+
+
+#
+# create test cases for every possibility listed in step 2 above
+#
+tests = []
+# append the example we created above
+tests.append(test_case)
+# create a test case for if num occurs in the middle of cards
+tests.append({
+    'input': {
+        'cards': [13, 11, 10, 7, 4, 3, 1, 0],
+        'num': 1
+    },
+    'output': 6
+})
+# create a test case for if num is the first element in cards
+tests.append({
+    'input': {
+        'cards': [4, 2, 1, -1],
+        'num': 4
+    },
+    'output': 0
+})
+# create test case for if num is last element in cards
+tests.append({
+    'input': {
+        'cards': [4, 7, 2, -6, 1],
+        'num': 1
+    },
+    'output': 4
+})
+# create test case for if cards contains only num as element
+tests.append({
+    'input': {
+        'cards': [8],
+        'num': 8
+    },
+    'output': 0
+})
+# create test case for if cards does not contain num
+# the problem statement doesnt specify what to do in this scenario --> make a reasonable assumption
+tests.append({
+    'input': {
+        'cards': [4, 7, 1, 0, -9],
+        'num': 5
+    },
+    'output': -1
+})
+# create test case for if cards is empty
+tests.append({
+    'input': {
+        'cards': [],
+        'num': 2
+    },
+    'output': -1
+})
+# create test case for if cards contains repeating numbers
+tests.append({
+    'input': {
+        'cards': [3, 3, 3, 8, 8, 1, 9, 3, 6, 6, 6],
+        'num': 1
+    },
+    'output': 5
+})
+# create test case for if cards contains num more than once
+# we assume our function will return the first instance of num
+tests.append({
+    'input': {
+        'cards': [3, 8, 3, 9, 0],
+        'num': 3
+    },
+    'output': 0
+})
+
+# view full list of test cases
+# a good number of test cases to produce is 3-5
+# with lots of practice, you should be able to complete step 1 & 2 in 2-3 minutes
+print(tests)
