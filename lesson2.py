55067f6528db0a28615a2d8eedd08d8937d0e6e1
Veronica Burgess <burgess.veronicas@gmail.com>
2023-12-13 06:57:14 -0700

    Example 2: calculating height and size of a binary tree
    Of "Data Structures and Algorithms in Python" from freeCodeCamp.org

--- lesson2.py
+++ lesson2.py
@@ -497,3 +497,20 @@ def traverse_postorder(node):
             [node.key])
 
 print(traverse_postorder(tree2))
+
+
+# calculate height/depth of a binary tree
+def tree_height(node):
+    if node is None:
+        return 0
+    return 1 + max(tree_height(node.left), tree_height(node.right))
+
+print(tree_height(tree2))
+
+# count number of nodes in a binary tree
+def tree_size(node):
+    if node is None:
+        return 0
+    return 1 + tree_size(node.left) + tree_size(node.right)
+
+print(tree_size(tree2))
