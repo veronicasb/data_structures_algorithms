d1635ea1857da2ed6801ac7bb50da6cd03b195fd
Veronica Burgess <burgess.veronicas@gmail.com>
2023-12-27 11:52:43 -0700

    Lesson 4: Finished strategy step 1
    Of "Data Structures and Algorithms in Python" from freeCodeCamp.org

---
+++ lesson4_recursion_memoization_dynamic.py
@@ -0,0 +1,57 @@
+"""
+2 common problems in dynamic programming: 
+
+1. longest common subsequence
+2. knapsack
+
+"""
+
+# Longest Common Subsequence
+
+"""
+PROBLEM
+
+Write a function to find the length of the longest common subsequence between
+two sequences.
+
+A sequence is a group of items with deterministic ordering.
+
+A subsequence is a sequence obtained by deleting zero or more elements from 
+another sequence.
+
+"""
+
+"""
+METHOD
+
+1. State problem clearly and indentify inputs and outputs
+2. Brainstorm example inputs and outputs, especially ones that cover edge cases
+3. Brainstorm a correct solution for the problem stated in plain English
+4. Implement solution and test example inputs. Fix bugs.
+5. Analyze complexity and indentify inefficiencies.
+6. Improve inefficiencies, then repeat 3-6
+
+"""
+
+
+# Step 1
+
+"""
+A function that identifies the longest common subsequence between 2 sequences, 
+then counts and returns the number of characters in that subsequence.
+
+Parameters: 
+1. seq1: a sequence (e.g. "serendipitous")
+2. seq2: another sequence (e.g. "precipitation")
+
+Return:
+1. len_lcs: length of longest common subsequence (e.g. 7)
+
+"""
+
+# After step 1 is complete, we can create a signature for our function
+def len_lcs(seq1, seq2):
+    pass
+
+
+# Step 2
