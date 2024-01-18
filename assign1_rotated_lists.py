0877f4e00572659e02009a4eebd9020eb2e238cc
Veronica Burgess <burgess.veronicas@gmail.com>
2024-01-18 13:11:58 -0700

    Assign 1: finished strategy step 1
    Of "Data Structures and Algorithms in Python" from freeCodeCamp.org

---
+++ assign1_rotated_lists.py
@@ -0,0 +1,52 @@
+
+"""
+PROBLEM
+
+You are given a list of numbers, obtained by rotating a sorted list an unknown number of 
+times. Write a function to determine the minimum number of times the original sorted list 
+was rotated to obtain the given list. Your function should have the worst-case complexity of 
+O(logN), where N is the length of the list. You can assume that all the numbers in the list 
+are unique.
+
+Example: The list [5, 6, 9, 0, 2, 3, 4] was obtained by rotating the sorted list 
+[0, 2, 3, 4, 5, 6, 9] 3 times.
+
+We define "rotating a list" as removing the last element of the list and adding it before the 
+first element.
+
+"Sorted list" refers to a list where the elements are arrange in increasing order.
+
+"""
+
+
+# STATE PROBLEM CLEARLY. IDENTIFY INPUT AND OUTPUT FORMATS.
+
+"""
+A function that takes a rotated list, then counts the minimum number of times a sorted list 
+had to be rotated to result in that given rotated list.
+
+Inputs:
+1. rotated_ls - a rotated list (ex. [5, 6, 9, 0, 2, 3, 4] )
+
+Outputs:
+1. rotations - an int as the number of times an original sorted list was rotated to 
+match input nums (ex. if our original sorted list is [0, 2, 3, 4, 5, 6, 9], 
+our output would be "3") 
+
+"""
+
+# our function signature
+
+def rotated_list(rotated_ls):
+    pass
+
+
+# COME UP WITH EXAMPLE INPUTS AND OUTPUTS. TRY TO COVER EDGE CASES.
+
+# COME UP WITH A CORRECT SOLUTION. STATE IT IN PLAIN ENGLISH.
+
+# IMPLEMENT THE SOLUTION AND TEST IT USING EXAMPLE INPUTS. FIX BUGS.
+
+# ANALYZE THE ALGORITHMS COMPLEXITY AND IDENTIFY INEFFICIENCIES.
+
+# APPLY RIGHT TECHNIQUE TO OVERCOME INEFFICIENCY. REPEAT STEPS 3 - 6.
