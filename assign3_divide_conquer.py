18331cb8082fbead5951dd0302a7031fc7aa8b1c
Veronica Burgess <burgess.veronicas@gmail.com>
2024-01-29 02:22:22 -0700

    Assign 3: completed strategy step 1
    Of "Data Structures and Algorithms in Python" from freeCodeCamp.org

---
+++ assign3_divide_conquer.py
@@ -0,0 +1,41 @@
+"""
+PROBLEM
+
+Given 2 polynomials represented by 2 lists, write a function that efficiently multiplies 
+the polynomials. Represent the solution as a list as well.
+
+"""
+
+
+# STATE THE PROBLEM CLEARLY. IDENTIFY INPUT AND OUTPUT FORMATS.
+
+"""
+Function that efficiently multiplies 2 polynomials and represents the solution as a list.
+
+Input:
+
+1. poly1 = a list of integers representing a polynomial (e.g. [2, 0, 5, 7] represents 
+2 + 5x^2 + 7x^3)
+
+2. poly2 = a list of integers representing a polynomial (e.g. [3, 4, 2] represents 
+3 + 4x + 2x^2)
+
+Output:
+1. answer = a list of integers representing a polynomial (e.g. [6, 8, 19, 41, 38, 14] 
+represents 6 + 8x + 19x^2 + 41x^3 + 38x^4 + 14x^5)
+
+"""
+
+def mulitply_poly(poly1, poly2):
+    pass
+
+
+# BRAINSTORM EXAMPLE INPUTS AND OUTPUTS
+
+# BRAINSTORM CORRECT SOLUTION IN PLAIN ENGLISH
+
+# IMPLEMENT SOLUTIONS AND TEST. FIX BUGS
+
+# ANALYZE ALGORITHM COMPLEXITY AND IDENTIFY INEFFICIENCIES
+
+# OVERCOME INEFFICIENCY. REPEAT 3 - 6
