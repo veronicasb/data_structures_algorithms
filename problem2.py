80dfabcbd6903c803372ef4818cee7d3dc8f9b62
Veronica Burgess <burgess.veronicas@gmail.com>
2024-02-05 16:26:27 -0700

    Problem 2: finished step 1 - state problem with input/output formats
    Of "Data Structures and Algorithms in Python" from freeCodeCamp.org

---
+++ problem2.py
@@ -0,0 +1,37 @@
+"""
+PROBLEM
+
+Given 2 strings, A and B, find the minimumu number of steps required to convert 
+A to B. The following 3 operations are permitted on a word:
+
+1. Insert a character
+2. Delete a character
+3. Replace a character
+
+"""
+
+
+# STATE PROBLEM AND DETERMINE INPUT/OUTPUT FORMATS
+
+"""
+A function that finds the minimum number of steps required to transform one string 
+into another string.
+
+Input:
+1. string_a - a string to transform
+2. string_b - a string to transform to
+
+Output:
+1. steps - an integer as the number of steps required to convert one string to another
+
+"""
+
+def conversion_steps(string_a, string_b):
+    pass
+
+
+# BRAINSTORM EXAMPLE INPUTS AND OUTPUTS 
+
+"""
+
+"""
